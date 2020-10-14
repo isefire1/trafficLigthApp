f4247a713b4f897537422fcace5e7d5887d34728
isefire1 <isefire@bk.ru>
2020-10-14 18:56:42 +0300

    final

--- trafficLigthApp/ViewController.swift
+++ trafficLigthApp/ViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum DisplayLight {
+    case red, yellow, green
+}
 
+class ViewController: UIViewController {
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var buttonStart: UIButton!
+    
+    private var displayLight = DisplayLight.red
+    private let lightOn: CGFloat = 1
+    private let lightOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        buttonStart.layer.cornerRadius = 10
+        
+        redLight.alpha = lightOff
+        yellowLight.alpha = lightOff
+        greenLight.alpha = lightOff
     }
-
-
+    
+    @IBAction func toggleLightButtonPresed() {
+        buttonStart.setTitle("NEXT", for:.normal)
+        
+        switch displayLight {
+        case .red:
+            greenLight.alpha = lightOff
+            redLight.alpha = lightOn
+            displayLight = .yellow
+        case .yellow:
+            redLight.alpha = lightOff
+            yellowLight.alpha = lightOn
+            displayLight = .green
+        case .green:
+            yellowLight.alpha = lightOff
+            greenLight.alpha = lightOn
+            displayLight = .red
+        }
+    }
+    
 }
 
